5719967a9d09b3edd4e24c919506d654e78e4765
Nobody <17411508+Nobodyno-boop@users.noreply.github.com>
2022-01-07 10:48:27 +0100

    add comment to ForToken.php

--- src/Lexer/tokens/ForToken.php
+++ src/Lexer/tokens/ForToken.php
@@ -4,18 +4,24 @@ namespace Clownerie\ClownView\Lexer\tokens;
 
 use Clownerie\ClownView\Parser\Parser;
 
+
+/**
+ *  Exemple:
+ *
+ *  {%for item in items %}
+ *      {% item %}
+ *  {% endfor %}
+ */
 class ForToken extends AbstractToken
 {
 
     public function execute(Parser $parser): void
     {
-//        dump(spl_object_id($this));
-//        dump($parser->getLine());
         $start = $parser->getCursor();
         $arr = $parser->getToken();
         $str = trim(str_replace(["for", "in"], "", $arr["var"]));
         $ex = explode(" ", $str);
-        $varname = $ex[count($ex)-1];
+        $varname = $ex[count($ex)-1]; // get the variable name | like the exemple its items
 
         $var = $parser->getVariables()["$varname"];
 
@@ -23,6 +29,7 @@ class ForToken extends AbstractToken
         $end = $parser->nextToken("endfor");
         $end['islink'] = $start;
         $parser->updateToken($end, $end['cursor']);
+        // createBlock is a copy of lines for create the "block template"
         $block = $parser->createBlock($start+1, $end["cursor"]- $start - 1);
 
         $ch = [];
@@ -33,13 +40,14 @@ class ForToken extends AbstractToken
             $blo = array_map(function ($value) use ($item,  $parser){
                 if(is_array($value)){
                     $value["content"] = str_replace($value["content"], $item, $value["content"]);
+                    $value["cursor"] = -1; // to avoid error
                     return $value;
                 }
                 return $value;
             }, $clone);
             $ch[] = $blo;
         }
-
+        // merge array (by cutting -> merge all)
         $parser->replaceToken($start+1, count($block), $ch);
 
     }
